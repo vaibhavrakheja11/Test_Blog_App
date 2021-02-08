7908c56250fff85493d8480f0213b0a4684493c3
vaibhavrakheja11 <vaibhavrakheja11@gmail.com>
2021-02-07 19:34:37 -0500

    Setup Default branch

--- Test_Blog_App/Assets/Scripts/Controller/AppController.cs
+++ Test_Blog_App/Assets/Scripts/Controller/AppController.cs
@@ -11,9 +11,10 @@ namespace BlogApp
     ///  All UI controllers to access models via an instance of this class
     /// Models to provide information to UI view controller using an instance of this class
     ///</summary>
-    
+
     public class AppController : MonoBehaviour
     {
+        
         private static AppController _instance;
 
         public static AppController Instance { get { return _instance; } }
